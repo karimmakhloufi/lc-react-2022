296cde92a373e60a3a27138b06119b3cfa7cf188
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-08-27 21:32:13 +0200

    Used useEffect and useState and axios to fetch wilders from the node rest API

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,19 @@
+import axios from "axios";
 import Wilder from "../components/Wilder";
+import { useEffect, useState } from "react";
 
-const wildersData = [
-  {
-    name: "Ringo",
-    city: "London",
-    skills: [
-      { title: "JS", votes: 10 },
-      { title: "React", votes: 8 },
-    ],
-  },
-  {
-    name: "John",
-    city: "Paris",
-    skills: [
-      { title: "PHP", votes: 9 },
-      { title: "Symfony", votes: 9 },
-    ],
-  },
-  {
-    name: "George",
-    city: "Berlin",
-    skills: [
-      { title: "Ruby", votes: 10 },
-      { title: "JS", votes: 8 },
-    ],
-  },
-  {
-    name: "Paul",
-    city: "Reims",
-    skills: [
-      { title: "C++", votes: 10 },
-      { title: "Rust", votes: 8 },
-    ],
-  },
-];
 const Home = () => {
+  const [wilders, setWilders] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const wilders = await axios.get("http://localhost:5000/api/wilder");
+      console.log(wilders.data);
+      setWilders(wilders.data);
+    };
+
+    fetchData();
+  }, []);
   return (
     <div>
       <header>
@@ -45,9 +24,9 @@ const Home = () => {
       <main className="container">
         <h2>Wilders</h2>
         <section className="card-row">
-          {wildersData.map((wilder, index) => (
+          {wilders.map((wilder) => (
             <Wilder
-              key={index}
+              key={wilder.id}
               name={wilder.name}
               city={wilder.city}
               skills={wilder.skills}
